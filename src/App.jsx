0bea2ee21793832a1272d19818d5863b6a7749a7
Habibur Rahman <h.r.sihab155@gmail.com>
2024-06-06 18:48:52 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Habibur Rahman",
+  title: "Web Developer",
+  email: "h.r.sihab155@gmail.com",
+  gitHub: "https://github.com/habib-153",
+  instagram: "https://www.instagram.com/h.rahman153/",
+  linkedIn: "https://www.linkedin.com/in/habiburrahman153/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
